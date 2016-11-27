d00c04033e61ac4719df222109168586e4112bed
Oskari Ruutiainen <ozquli@gmail.com>
2016-11-27 15:32:15 +0200

    added underscore

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var UntappdClient = require("./node_modules/node-untappd/UntappdClient",false);
+var _ = require('underscore');
 
 // Definitions
 var clientId = "[ your api key goes here ]";
@@ -9,7 +10,7 @@ var accessToken = "[ your access token goes here ]";
 var debug = false;
 
 //The user we want to lookup for this example.
-var lookupuser = "jevli";	
+var lookupuser = "jevli";
 
 // Create Client
 var untappd = new UntappdClient(debug);
@@ -20,32 +21,31 @@ untappd.setAccessToken(accessToken); // TODO add accessToken adding LATER get ac
 // repeat 15 minutes
 untappd.activityFeed(function(err,obj){
   //if (debug) console.log(obj, err);
-  
+
   var afterwork;
   // Check what counts is really | either this or items.size etc
   /*if (obj & obj.checkins.count > 0) {
-  	for( var item of obj.items ) {
-    	afterwork.push({
-        'time': item.created_at,
-        'vid': item.venue.venue_id,
-        'name': item.venue.venue_name,
-        'uid': item.user.uid,
-        'name': item.user.first_name + ' ' + item.user.last_name 
-      });
-    }*/
+   for( var item of obj.items ) {
+   afterwork.push({
+   'time': item.created_at,
+   'vid': item.venue.venue_id,
+   'name': item.venue.venue_name,
+   'uid': item.user.uid,
+   'name': item.user.first_name + ' ' + item.user.last_name
+   });
+   }*/
 
-    var testi = [
-      {'vid': 1, 'name': 'Foo'},
-      {'vid': 1, 'name': 'LoL'},
-      {'vid': 2, 'name': 'Bar'},
-      {'vid': 3, 'name': 'user1'},
-      {'vid': 3, 'name': 'user2'}
-    ]
-        
-    testi.map( venue => { venue.count = testi.filter( elem => elem.vid === venue.vid ).length })
-    var tulos = testi.filter( venue => venue.count > 1);
-    console.log(tulos);
+  var testi = [
+    {'vid': 1, 'name': 'Foo'},
+    {'vid': 1, 'name': 'LoL'},
+    {'vid': 2, 'name': 'Bar'},
+    {'vid': 3, 'name': 'user1'},
+    {'vid': 3, 'name': 'user2'}
+  ]
 
-    //}
-  
+  testi = _.groupBy(testi, function(elem) {
+    return elem.vid;
+  });
+  console.log(testi);
+
 }, {'limit': 50});
