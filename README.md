f19b52a13fd3d61767b81619363f5d85bff984d0
Oskari Ruutiainen <ozquli@gmail.com>
2016-11-29 22:45:18 +0200

    some stuff

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # afterwork-alert
+This is Untappd-Slack integration for alerting Slack-users of afterwork going on somewhere.
+Untappd polling periodically and if more than one friend is drinking within small enough period of time, notifies to Slack that afterwork is being held.
+
+* Config includes loopingTime as well as bot name and channels of multiple cities, based on venue locations
+* more to come...
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var UntappdClient = require("node-untappd");
 var Slack = require('slack-node');
-var slack = new Slack();
 var _ = require('underscore');
 var moment = require('moment');
 var config = require('./config');
@@ -22,90 +21,94 @@ if (process.argv.length > 2 && process.argv[2] == 'debug') {
   debug = true;
 }
 
-// Create Client
+// Create Untappd Client
 var untappd = new UntappdClient(debug);
 untappd.setClientId(clientId);
 untappd.setClientSecret(clientSecret);
 untappd.setAccessToken(accessToken); // TODO add accessToken adding LATER get accessToken
+// Create Slack Client
+var slack = new Slack();
+slack.setWebhook(slackWebhookURL); // set url
 
-// setInterval(get, 1000);
-get();
-function get() {
-untappd.activityFeed(function(err,obj){
-  if (debug) console.log(obj, err);
+var getAfterworkFeed = function(cb) {
+  untappd.activityFeed(function (err, obj) {
+    if (debug) console.log(obj, err);
 
-  var afterwork = [];
+    var afterwork = [];
 
-  // Check what counts is really | either this or items.size etc
-  if (obj && obj.response && obj.response.checkins.count > 0) {
-  
-    var items = obj.response.checkins.items;
+    // Check what counts is really | either this or items.size etc
+    if (obj && obj.response && obj.response.checkins.count > 0) {
 
-    for( var item of items ) {
-      afterwork.push({
-        'time': item.created_at,
-        'vid': item.venue !== undefined ? item.venue.venue_id : undefined,
-        'vname': item.venue !== undefined ? item.venue.venue_name : undefined,
-        'city': item.venue !== undefined && item.venue.location !== undefined ? item.venue.location.venue_city : undefined,
-        'uid': item.user.uid,
-        'name': item.user.first_name + ' ' + item.user.last_name
-      });
-    }
+      var items = obj.response.checkins.items;
 
-    var current_time = moment().subtract(2, 'd'); // TODO: change to timestamp
-
-    /*var testi = [
-      {'vid': 1, 'vname': 'bar1', 'name': 'user1', 'city': 'Tampere', 'time': 'Sun, 27 Nov 2016 15:51:00 +0000'},
-      {'vid': 1, 'vname': 'bar1', 'name': 'user2', 'city': 'Tampere', 'time': 'Sun, 27 Nov 2016 15:51:00 +0000'},
-      {'vid': 2, 'vname': 'bar2', 'name': 'user3', 'city': 'Tampere', 'time': 'Sun, 27 Nov 2016 15:51:00 +0000'},
-      {'vid': 3, 'vname': 'bar3', 'name': 'user4', 'city': 'Tampere', 'time': 'Sun, 20 Nov 2016 13:51:00 +0000'},
-      {'vid': 3, 'vname': 'bar3', 'name': 'user5', 'city': 'Tampere', 'time': 'Sun, 27 Nov 2016 13:51:00 +0000'}
-    ]*/
-
-    afterwork = _.chain(afterwork)
-      .filter(function(elem) {
-        return moment(elem.time, 'ddd, D MMM YYYY HH:mm:ss +0000').isAfter(current_time);
-      })
-      .filter(function(elem) {
-        return elem.vid;
-      })
-      .groupBy(function(elem) {
-        return elem.vid;
-      })
-      .values()
-      .map(function(elem) {
-        return _.uniq(elem, false, function(a) {
-          return a.uid + "--" + a.vid;
+      for (var item of items) {
+        afterwork.push({
+          'time': item.created_at,
+          'vid': item.venue !== undefined ? item.venue.venue_id : undefined,
+          'vname': item.venue !== undefined ? item.venue.venue_name : undefined,
+          'city': item.venue !== undefined && item.venue.location !== undefined ? item.venue.location.venue_city : undefined,
+          'uid': item.user.uid,
+          'name': item.user.first_name + ' ' + item.user.last_name
         });
-      })
-      .filter(function(elem) {
-        return elem.length > 1;
-      })
-      .value();
-
-    if (debug) console.log(afterwork);
-
-    // Slack stuff
-    slack.setWebhook(slackWebhookURL); // set url
-    // for every venue, send message
-    for (let venue of afterwork) {
-      // build persons string
-      var persons = "";
-      for(let checkin of venue) {
-        persons += checkin.name + ' ';
       }
-      persons = persons.slice(0, -1);
-      // build payload
-      var payload = {
-        'text': venue.length + ' henkilöä afterworkilla ravintolassa ' + venue[0].vname + ' (' + persons + ')',
-        'channel': channels[venue[0].city],
-        'username': botname
-      }
-      slack.webhook(payload, function(err, response) {
-        if (debug) console.log(response);
-      })
+
+      var earliest_allowed_checkin = moment().subtract(2, 'd'); // TODO: change to timestamp
+
+      afterwork = _.chain(afterwork)
+        // Checkin has to be new enough
+        .filter(function (elem) {
+          return moment(elem.time, 'ddd, D MMM YYYY HH:mm:ss +0000').isAfter(earliest_allowed_checkin);
+        })
+        // Has to have venue
+        .filter(function (elem) {
+          return elem.vid;
+        })
+        .groupBy(function (elem) {
+          return elem.vid;
+        })
+        .values()
+        // Exludes same users drinks in same venue
+        .map(function (elem) {
+          return _.uniq(elem, false, function (a) {
+            return a.uid + "--" + a.vid;
+          });
+        })
+        // Has to have more than one user in same bar
+        .filter(function (elem) {
+          return elem.length > 1;
+        })
+        .value();
+
+      if (debug) console.log(afterwork);
+      cb(afterwork);
     }
-  }
+  });
+};
 
-});
-}
+var parseAfterWorkAndSendToSlack = function(afterwork) {
+  // for every venue, send message
+  for (let venue of afterwork) {
+    // build persons string
+    var persons = "";
+    for(let checkin of venue) {
+      persons += checkin.name + ' ';
+    }
+    persons = persons.slice(0, -1);
+    // build payload
+    var payload = {
+      'text': venue.length + ' henkilöä afterworkilla ravintolassa ' + venue[0].vname + ' (' + persons + ')',
+      'channel': channels[venue[0].city],
+      'username': botname
+    }
+    slack.webhook(payload, function(err, response) {
+      if (debug) console.log(response);
+    })
+  }
+};
+
+// Actually call methods
+getAfterworkFeed(parseAfterWorkAndSendToSlack);
+
+// Custom Slack Bot Stuff
+function startBot() {
+};
