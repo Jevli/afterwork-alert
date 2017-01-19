9c04eab60de1c1986504baf6bfd1e5f725f13355
Joni Laurila <joni.laurila@gofore.com>
2017-01-19 09:36:20 +0200

    Some updates for servlerless: - added resources - added profile - added table - fixed bugs Also make login logic more 'solid'

---
+++ serverless/src/handler.js
@@ -0,0 +1,36 @@
+'use strict';
+var doc = require('dynamodb-doc');
+var dynamodb = new doc.DynamoDB();
+
+exports.handler.login = (event, context, callback) => {
+    var table = 'users';
+    var loginInfo = JSON.parse(event.body);
+    var response = {
+        headers: {
+            "Access-Control-Allow-Origin" : "*"
+        }
+    };
+    if (loginInfo.uname && loginInfo.pwd) {
+        var parameters;
+        // TODO query parameters
+        dynamodb.query({
+            "TableName": table
+        }, function (err, data) {
+            if (err) {
+                response['statusCode'] = 404;
+                response['body'] = JSON.stringify({"error": "error", error: err});
+                callback(response, null);
+                context.done();
+            } else {
+                response['statusCode'] = 200
+                response['body'] = JSON.stringify({"message": "succes"});
+                callback(null, response)
+                context.succeed();
+            }
+        });
+        
+    } else {
+        callback("Missing Username or Password", null);
+        context.done();
+    }
+};
